59838451fd715c7a50804520db0b43957943ccee
黄荣君 <l1091462907@gmail.com>
2021-08-04 12:50:24 +0000

    !20 IUserLoginCheck 删除获取用户手机号的函数--配套地藤主帐号升级使用

--- summer-mis/src/main/java/cn/cerc/mis/core/IUserLoginCheck.java
+++ summer-mis/src/main/java/cn/cerc/mis/core/IUserLoginCheck.java
@@ -5,13 +5,10 @@ import cn.cerc.db.core.IHandle;
 public interface IUserLoginCheck extends IHandle {
 
     // 登录验证
-    String createToken(String userCode, String password, String machineCode, String clientIP, String language);
-
-    // 返回手机号
-    String getMobile();
+    String getToken(String userCode, String password, String machineCode, String clientIP, String language);
 
     // 通过手机号获取帐号
-    String getUserCode(String mobile);
+    String getUserCode(String mobile);// FIXME 该方法不需要，所有关联的登录服务要调整
 
     // 错误消息
     String getMessage();
