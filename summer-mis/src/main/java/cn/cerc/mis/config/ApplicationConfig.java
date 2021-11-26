c4de9dc4017b3299f19cf9c62773f8baac704cec
李远 <460289628@qq.com>
2021-11-26 03:39:27 +0000

    !74 清理警告代码

--- summer-mis/src/main/java/cn/cerc/mis/config/ApplicationConfig.java
+++ summer-mis/src/main/java/cn/cerc/mis/config/ApplicationConfig.java
@@ -4,14 +4,6 @@ import cn.cerc.core.LanguageResource;
 
 public class ApplicationConfig {
 
-    @Deprecated
-    public static final String App_Path = "/public/";
-
-    @Deprecated
-    public static String rewrite(String form) {
-        return ApplicationConfig.App_Path + form;
-    }
-
     // TODO: 2021/3/11 后需要改为使用配置文件的方式
     public static final String PATTERN_CN = "0.##";
     public static final String PATTERN_TW = ",###.####";
