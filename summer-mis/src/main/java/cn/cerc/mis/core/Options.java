b1930705ed60484b79817c8c347808e6373fa23a
张弓 <sz9214e@qq.com>
2022-01-03 21:27:03 +0800

    Create Options.java

---
+++ summer-mis/src/main/java/cn/cerc/mis/core/Options.java
@@ -0,0 +1,32 @@
+package cn.cerc.mis.core;
+
+import cn.cerc.db.core.IHandle;
+import cn.cerc.db.core.IOption;
+import cn.cerc.db.core.KeyValue;
+
+public class Options {
+
+    public static final String ON = "on";
+    public static final String OFF = "off";
+
+    public static KeyValue get(IHandle handle, Class<? extends IOption> clazz) {
+        IOption option = Application.getContext().getBean(clazz);
+        return new KeyValue(option.getValue(handle));
+    }
+
+    public static KeyValue get(IHandle handle, Class<? extends IOption> clazz, String def) {
+        IOption option = Application.getContext().getBean(clazz);
+        return new KeyValue(option.getValue(handle, def));
+    }
+
+    public static boolean isOn(IHandle handle, Class<? extends IOption> clazz) {
+        IOption option = Application.getContext().getBean(clazz);
+        return cn.cerc.mis.core.Options.ON.equals(option.getValue(handle, ""));
+    }
+
+    public static boolean isOff(IHandle handle, Class<? extends IOption> clazz) {
+        IOption option = Application.getContext().getBean(clazz);
+        return cn.cerc.mis.core.Options.OFF.equals(option.getValue(handle, ""));
+    }
+
+}
