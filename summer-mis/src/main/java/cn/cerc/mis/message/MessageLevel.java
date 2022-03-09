65f0b6b0ab9d47adeb23a27bdcfb63dae140528f
itjun <l1091462907@gmail.com>
2022-03-09 05:59:43 +0000

    !127 MessageLevel 消息等级增加存活时间--按天为单位

--- summer-mis/src/main/java/cn/cerc/mis/message/MessageLevel.java
+++ summer-mis/src/main/java/cn/cerc/mis/message/MessageLevel.java
@@ -1,11 +1,36 @@
 package cn.cerc.mis.message;
 
 public enum MessageLevel {
-    General, // 通用消息
-    Great, // 重要消息，需要在首页予滚动展示
-    Grave, // 紧急消息，需要弹窗提示
-    Logger, // 日志类消息，默认为已读
-    Service, // 后台任务
-    Printer, // 打印任务
-    Export // 导出消息
+
+    // 通用消息-3个月
+    General(3 * 33),
+
+    // 重要消息，需要在首页予滚动展示-永久
+    Great(0),
+
+    // 紧急消息，需要弹窗提示-3年
+    Grave(3 * 12 * 33),
+
+    // 日志类消息，默认为已读-1年
+    Logger(12 * 33),
+
+    // 后台任务-33天
+    Service(33),
+
+    // 打印任务-3天
+    Printer(3),
+
+    // 导出消息-2天
+    Export(2);
+
+    private int day;
+
+    private MessageLevel(int day) {
+        this.day = day;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
 }
