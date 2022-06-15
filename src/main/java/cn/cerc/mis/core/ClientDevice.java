c9e033b723db4b9b9ff94f4f9a17d4a5f671f1ff
张弓 <sz9214e@qq.com>
2022-06-15 21:27:56 +0800

    Update ClientDevice.java

--- src/main/java/cn/cerc/mis/core/ClientDevice.java
+++ src/main/java/cn/cerc/mis/core/ClientDevice.java
@@ -6,7 +6,7 @@ import cn.cerc.db.core.IHandle;
 public class ClientDevice extends AppClient {
     private static final long serialVersionUID = 3637149857407787553L;
     public static final String device_pc = pc;
-    public static final String device_tv = tv;
+    public static final String device_Kanban = kanban;
     public static final String deviceId_key = CLIENT_ID;
     public static final String deviceType_key = DEVICE;
 
