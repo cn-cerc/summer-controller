007ad038169d55b7758017778df903ac405b3c27
张弓 <sz9214e@qq.com>
2022-09-19 22:23:39 +0800

    Update ModuleFormImpl.java

--- src/main/java/cn/cerc/mis/custom/ModuleFormImpl.java
+++ src/main/java/cn/cerc/mis/custom/ModuleFormImpl.java
@@ -1,10 +1,10 @@
 package cn.cerc.mis.custom;
 
-public interface ModuleFormImpl {
+public interface ModuleFormImpl extends CustomFormImpl {
     /**
      * 
      * @return 取得模组说明
      */
     public String getModuleReadme();
-    
+
 }
