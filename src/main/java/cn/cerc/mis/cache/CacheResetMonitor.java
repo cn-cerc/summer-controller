7944a5896724f1c1151cc19d566a9dc75817ea30
张弓 <sz9214e@qq.com>
2022-11-02 16:16:34 +0800

    将Jedis改为使用Redis

--- src/main/java/cn/cerc/mis/cache/CacheResetMonitor.java
+++ src/main/java/cn/cerc/mis/cache/CacheResetMonitor.java
@@ -1,14 +1,13 @@
 package cn.cerc.mis.cache;
 
-import cn.cerc.db.redis.JedisFactory;
-import redis.clients.jedis.Jedis;
+import cn.cerc.db.redis.Redis;
 
 public class CacheResetMonitor extends Thread {
     private SubCacheEvent monitor = new SubCacheEvent();
 
     @Override
     public void run() {
-        try (Jedis jedis = JedisFactory.getJedis()) {
+        try (Redis jedis = new Redis()) {
             if (jedis != null)
                 jedis.subscribe(monitor, MemoryListener.CacheChannel);
         }
