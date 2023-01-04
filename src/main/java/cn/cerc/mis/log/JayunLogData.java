241f78ac925d758289837941221573fe3981c959
谢俊 <xj_422171988@163.com>
2023-01-04 09:30:35 +0800

    Update JayunLogData.java

--- src/main/java/cn/cerc/mis/log/JayunLogData.java
+++ src/main/java/cn/cerc/mis/log/JayunLogData.java
@@ -4,10 +4,6 @@ import org.apache.log4j.Level;
 import org.apache.log4j.spi.LoggingEvent;
 
 public class JayunLogData {
-    /**
-     * 项目错误/警告日志表
-     */
-    public static final String T_Project_Log = "t_project_log";
     public static final int Info = 0;
     public static final int Warn = 1;
     public static final int Error = 2;
