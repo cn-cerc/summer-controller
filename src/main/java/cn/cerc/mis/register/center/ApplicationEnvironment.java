4d8d6923f3282a517d506e744c0237463e69f390
黄荣君 <l1091462907@gmail.com>
2023-05-12 14:50:32 +0000

    !229 改进 => 系统启动时将主机和应用的信息写入 zookeeper

---
+++ src/main/java/cn/cerc/mis/register/center/ApplicationEnvironment.java
@@ -0,0 +1,116 @@
+package cn.cerc.mis.register.center;
+
+import java.io.File;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Map;
+import java.util.Properties;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+import cn.cerc.db.core.Utils;
+
+/**
+ * 应用环境变量加载和配置列表
+ */
+public class ApplicationEnvironment {
+
+    private static final Logger log = LoggerFactory.getLogger(ApplicationEnvironment.class);
+
+    /**
+     * 获取系统的环境变量
+     */
+    public static Map<String, String> getenv() {
+        return System.getenv();
+    }
+
+    /**
+     * 获取虚拟机 系统属性
+     */
+    public static Properties properteis() {
+        return System.getProperties();
+    }
+
+    /**
+     * 应用主机名称
+     */
+    public static String hostname() {
+        String hostname;
+        try {
+            InetAddress inet = InetAddress.getLocalHost();
+            hostname = inet.getHostName();
+        } catch (UnknownHostException e) {
+            log.error(e.getMessage(), e);
+            hostname = "";
+        }
+        return hostname;
+    }
+
+    /**
+     * 应用主机地址
+     * 
+     * docker run <br>
+     * --env DOCKER_HOST_IP=`hostname -I | awk '{print $1}'` \ <br>
+     * --env DOCKER_HOST_PORT=$port \
+     */
+    public static String hostIP() {
+        // docker 容器内就先读取环境变量，否则读取到的是内网地址，此变量需要建立容器时手动设置
+        String hostip = System.getenv("DOCKER_HOST_IP");
+        if (!Utils.isEmpty(hostip))
+            return hostip;
+
+        try {
+            InetAddress inet = InetAddress.getLocalHost();
+            hostip = inet.getHostAddress();
+        } catch (UnknownHostException e) {
+            log.error(e.getMessage(), e);
+        }
+        return hostip;
+    }
+
+    /**
+     * 应用主机端口
+     * 
+     * docker run <br>
+     * --env DOCKER_HOST_IP=`hostname -I | awk '{print $1}'` \ <br>
+     * --env DOCKER_HOST_PORT=$port \
+     */
+    public static String hostPort() {
+        // docker 容器内就先读取环境变量，否则读取到的是内网地址，此变量需要建立容器时手动设置
+        String httpPort = System.getenv("DOCKER_HOST_PORT");
+        if (!Utils.isEmpty(httpPort))
+            return httpPort;
+
+        try {
+            // Tomcat配置文件路径
+            String catalinaHome = System.getProperty("catalina.home");
+            String serverXmlPath = catalinaHome + File.separator + "conf" + File.separator + "server.xml";
+
+            // 创建DOM解析器
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            Document doc = factory.newDocumentBuilder().parse(serverXmlPath);
+
+            // 查找Connector元素
+            NodeList connectors = doc.getElementsByTagName("Connector");
+            for (int i = 0; i < connectors.getLength(); i++) {
+                Element connector = (Element) connectors.item(i);
+                String protocol = connector.getAttribute("protocol");
+                if (protocol != null && protocol.startsWith("HTTP")) {
+                    httpPort = connector.getAttribute("port");
+                    break;
+                }
+            }
+            return httpPort;
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+            return "unknow";
+        }
+    }
+
+}
--- src/main/java/cn/cerc/mis/register/center/ApplicationRegistryListener.java
+++ src/main/java/cn/cerc/mis/register/center/ApplicationRegistryListener.java
@@ -1,8 +1,5 @@
 package cn.cerc.mis.register.center;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-
 import org.apache.zookeeper.CreateMode;
 import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.ZooKeeper;
@@ -10,6 +7,7 @@ import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 
+import cn.cerc.db.core.Datetime;
 import cn.cerc.db.core.ServerConfig;
 import cn.cerc.db.core.Utils;
 import cn.cerc.db.zk.ZkServer;
@@ -29,22 +27,21 @@ public class ApplicationRegistryListener implements ApplicationListener<ContextR
         String appProduct = ServerConfig.getAppProduct();
         String appOriginal = ServerConfig.getAppOriginal();
         String appVersion = ServerConfig.getAppVersion();
+        String appName = ServerConfig.getAppName();
 
         String path = String.format("/%s/%s/%s/%s", appProduct, appVersion, appOriginal, "points");
         try {
             if (client.exists(path, false) == null)
                 server.create(path, "", CreateMode.PERSISTENT);
 
-            InetAddress addr = InetAddress.getLocalHost();
-            String ip = addr.getHostAddress();
-            String hostname = addr.getHostName();
-            String dockerIP = System.getenv("DOCKER_HOST");
-            if (!Utils.isEmpty(dockerIP))
-                ip = dockerIP;
-            server.create(String.join("/", path, Utils.getGuid()), String.join("-", hostname, ip, "alive"),
-                    CreateMode.EPHEMERAL);
-        } catch (KeeperException | InterruptedException | UnknownHostException e) {
+            String hostname = ApplicationEnvironment.hostname();
+            String ip = ApplicationEnvironment.hostIP();
+            String port = ApplicationEnvironment.hostPort();
+            String value = String.join(" ", new Datetime().toString(), appName, hostname, ip, port);
+            server.create(String.join("/", path, Utils.getGuid()), value, CreateMode.EPHEMERAL);
+        } catch (KeeperException | InterruptedException e) {
             e.printStackTrace();
         }
     }
+
 }
