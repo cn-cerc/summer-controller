1f167f15805c78d51f1652e868928ab25585a4cd
张弓 <sz9214e@qq.com>
2023-06-18 11:38:44 +0800

    增加注解的使用注意事项

--- src/main/java/cn/cerc/mis/security/Submenu.java
+++ src/main/java/cn/cerc/mis/security/Submenu.java
@@ -19,6 +19,7 @@ public @interface Submenu {
      * @return 返回父级菜单代码，可为空，其逻辑如下：</br>
      *         若标识为函数时，此值不应该设置，并默认等于其所在类</br>
      *         若标识为类时，默认等于Webform.module值，此值可被设置</br>
+     *         注意：实际在使用时，数据库中可以分行业设置并覆盖此值
      * 
      */
     String parent();
