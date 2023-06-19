65936abfaf1e1dc48300ebcf8cfc244e368e5ca2
张弓 <sz9214e@qq.com>
2023-06-19 19:54:17 +0800

    Update ModuleFormImpl.java

--- src/main/java/cn/cerc/mis/custom/ModuleFormImpl.java
+++ src/main/java/cn/cerc/mis/custom/ModuleFormImpl.java
@@ -1,10 +1,22 @@
 package cn.cerc.mis.custom;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public interface ModuleFormImpl extends CustomFormImpl {
     /**
      * 
      * @return 取得模组说明
      */
-    public String getModuleReadme();
+    String getModuleReadme();
 
+    /**
+     * 实际可用菜单比这个少的，删除这个列表<br>
+     * 实际可用菜单比这个多的，显示在这个返回值的后面
+     * 
+     * @return 菜单排序建议
+     */
+    default List<String> getMenuOrder() {
+        return new ArrayList<String>();
+    }
 }
