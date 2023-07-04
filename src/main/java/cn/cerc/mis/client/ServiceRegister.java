ea632891c9818f7c123a8bea5ed0aa53c57597fb
HUANG Rongjun <l1091462907@gmail.com>
2023-07-04 08:42:28 +0800

    Update ServiceRegister.java

--- src/main/java/cn/cerc/mis/client/ServiceRegister.java
+++ src/main/java/cn/cerc/mis/client/ServiceRegister.java
@@ -1,9 +1,16 @@
 package cn.cerc.mis.client;
 
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
 import java.util.Random;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import org.apache.zookeeper.CreateMode;
 import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.data.Stat;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeansException;
@@ -19,13 +26,29 @@ import cn.cerc.db.core.Datetime;
 import cn.cerc.db.core.ServerConfig;
 import cn.cerc.db.zk.ZkNode;
 import cn.cerc.mis.SummerMIS;
+import cn.cerc.db.zk.ZkServer;
 import cn.cerc.mis.register.center.ApplicationEnvironment;
 
 @Component
-public class ServiceRegister implements ApplicationContextAware, ApplicationListener<ContextRefreshedEvent> {
+public class ServiceRegister implements ApplicationContextAware, ApplicationListener<ContextRefreshedEvent>, Watcher {
     private static final Logger log = LoggerFactory.getLogger(ServiceRegister.class);
     private static final ClassConfig config = new ClassConfig(ServerConfig.class, SummerMIS.ID);
     private ApplicationContext context;
+
+    private static final String ROOT_PATH = String.format("/%s/%s/%s/host", ServerConfig.getAppProduct(),
+            ServerConfig.getAppVersion(), ServerConfig.getAppOriginal());
+
+    /**
+     * 子节点信息列表
+     */
+    private static final Map<String, Map<String, String>> intranetItems = new ConcurrentHashMap<>();
+
+    private static final Map<String, String> extranetItems = new ConcurrentHashMap<>();
+
+    /**
+     * 负载均衡计数器
+     */
+    private static final AtomicInteger atomic = new AtomicInteger();
 
     @Override
     public void onApplicationEvent(ContextRefreshedEvent event) {
@@ -75,26 +98,102 @@ public class ServiceRegister implements ApplicationContextAware, ApplicationList
      * @return 返回可用的服务地址
      */
     public ServiceSiteRecord getServiceHost(String industry) {
-        var rootPath = String.format("/%s/%s/%s/host", ServerConfig.getAppProduct(), ServerConfig.getAppVersion(),
+        String path = String.format("/%s/%s/%s/host", ServerConfig.getAppProduct(), ServerConfig.getAppVersion(),
                 industry);
-        var zk = ZkNode.get().server();
-        var list = zk.getNodes(rootPath);
-        if (list.size() > 0) {
-            log.debug("{} 有找到可用节点：{}", industry, list.size());
-            var nodeKey = list.get(new Random().nextInt(list.size()));
-            var nodeValue = zk.getValue(rootPath + "/" + nodeKey);
-            var node = new DataRow().setJson(nodeValue);
-            return new ServiceSiteRecord(true, industry, node.getString("host"));
-        } else {
-            var website = zk.getValue(rootPath);
-            log.warn("{} 没有有找到可用节点，改使用外网调用：{}", industry, website);
-            return new ServiceSiteRecord(false, industry, website);
+        ZkServer server = ZkNode.get().server();
+        Map<String, String> map;
+        try {
+            map = intranetItems.get(industry);
+            if (map == null) {
+                var list = server.client().getChildren(path, this);
+                map = new ConcurrentHashMap<String, String>();
+                for (var nodeKey : list) {
+                    var nodeValue = server.getValue(path + "/" + nodeKey);
+                    map.put(nodeKey, nodeValue);
+                }
+                intranetItems.put(industry, map);
+            }
+            if (map.size() > 0) {
+                log.debug("{} 有找到可用节点：{}", industry, map.size());
+                var list = map.keySet().toArray();
+                var nodeKey = list[new Random().nextInt(intranetItems.size())];
+                var nodeValue = map.get(nodeKey);
+                var node = new DataRow().setJson(nodeValue);
+                return new ServiceSiteRecord(true, industry, node.getString("intranet"));
+            } else {
+                var extranet = extranetItems.get(industry);
+                log.warn("{} 没有有找到可用节点，改使用外网调用：{}", industry, extranet);
+                return new ServiceSiteRecord(false, industry, extranet);
+            }
+        } catch (KeeperException | InterruptedException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e.getMessage());
         }
     }
 
     @Override
     public void setApplicationContext(ApplicationContext context) throws BeansException {
         this.context = context;
+
+    }
+
+    @Override
+    public void process(WatchedEvent event) {
+        var server = ZkNode.get().server();
+        String path = event.getPath();
+        try {
+            var client = server.client();
+            if (path.endsWith("/host")) {
+                if (event.getType() == Watcher.Event.EventType.NodeDataChanged) {
+                    Stat stat = client.exists(path, false);
+                    if (stat != null) {
+                        var value = new String(client.getData(path, this, stat), StandardCharsets.UTF_8);
+                        extranetItems.put(path, value);
+                    } else {
+                        extranetItems.remove(path);
+                    }
+                    // TODO 继续监听
+                }
+            } else {
+                if (event.getType() == Watcher.Event.EventType.NodeChildrenChanged) {
+                    Stat stat = client.exists(path, this);
+                    if (stat != null) {
+                        var list = server.client().getChildren(path, this);
+                        var map = new ConcurrentHashMap<String, String>();
+                        for (var nodeKey : list) {
+                            var nodeValue = server.getValue(path + "/" + nodeKey);
+                            map.put(nodeKey, nodeValue);
+                        }
+                        intranetItems.put(path, map);
+                    } else
+                        intranetItems.remove(path);
+                }
+            }
+        } catch (KeeperException | InterruptedException e) {
+            log.error(e.getMessage(), e);
+        }
+//        if (!path.contains(ROOT_PATH))
+//            return;
+//
+//        // 节点信息被删除，清空缓存列表
+//        if (event.getType() == Event.EventType.NodeDeleted) {
+//            return;
+//        }
+
+//        // 子节点列表有变化，重载缓存列表
+//        if (event.getType() == Watcher.Event.EventType.NodeChildrenChanged) {
+//            var value = server.getValue(node);
+//            if (value != null) {
+//                intranetItems.put(node, value);
+//                log.warn("节点 {} 值变更为 {}", node, value);
+//                server.watch(node, this); // 继续监视
+//            } else {
+//                log.error("节点 {} 不应该找不到！！！", node);
+//            }
+//        } else if (event.getType() == Watcher.Event.EventType.NodeDeleted) {
+//            intranetItems.remove(node);
+//            log.debug("节点 {} 已被删除！", node);
+//        }
 
     }
 }
