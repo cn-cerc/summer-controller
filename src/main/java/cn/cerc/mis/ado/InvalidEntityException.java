fc2e40633e8031a4d97b3a8dd3930001e32c95e4
HUANG Rongjun <l1091462907@gmail.com>
2023-07-10 21:13:53 +0800

    Update InvalidEntityException.java

--- src/main/java/cn/cerc/mis/ado/InvalidEntityException.java
+++ src/main/java/cn/cerc/mis/ado/InvalidEntityException.java
@@ -1,6 +1,11 @@
 package cn.cerc.mis.ado;
 
+/**
+ * 无效的实体异常类
+ */
 public class InvalidEntityException extends RuntimeException {
+
+    private static final long serialVersionUID = -4810945733324586294L;
 
     public InvalidEntityException(String message) {
         super(message);
