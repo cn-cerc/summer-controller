2d66751ca9eb97d99263a161deb285f034f839ed
HUANG Rongjun <l1091462907@gmail.com>
2023-07-12 10:24:22 +0800

    Create ServiceSiteRecord.java

---
+++ src/main/java/cn/cerc/mis/client/ServiceSiteRecord.java
@@ -0,0 +1,17 @@
+package cn.cerc.mis.client;
+
+public record ServiceSiteRecord(boolean isLan, String industry, String host) {
+    /**
+     * 
+     * @return 根据内网还是外网标识，返回相应的网址
+     */
+    public String website() {
+        // TODO 缺少原始产业别的判断
+        // 192.168.1.20:9101/services/
+        if (isLan())
+            return String.format("%s/services/", host());
+        else
+            // www.4plc.cn/services-csp/
+            return String.format("%s/services-%s/", host(), industry());
+    }
+}
