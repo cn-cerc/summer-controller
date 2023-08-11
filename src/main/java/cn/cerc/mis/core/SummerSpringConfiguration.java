c5a1e96ec7c8c3b77b629eae4988680cd6000b83
张建红 <sz9214e@qq.com>
2023-08-11 12:14:55 +0000

    !348 增加测试常用bean获取，方便复制

--- src/main/java/cn/cerc/mis/core/SummerSpringConfiguration.java
+++ src/main/java/cn/cerc/mis/core/SummerSpringConfiguration.java
@@ -1,5 +1,8 @@
 package cn.cerc.mis.core;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -7,5 +10,15 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan(basePackages = { "cn.cerc", "com.mimrc" })
 //@ImportResource("classpath*:summer-mis-spring.xml")
 public class SummerSpringConfiguration {
+
+    @Bean
+    public ISystemTable getSystemTable() {
+        return null;
+    }
+
+    @Bean
+    public HttpServletRequest getHttpServletRequest() {
+        return null;
+    }
 
 }
