847c13df4a139254370ccae2c29c5b952a7ffd1b
胡杰 <h2syj.zzzz@gmail.com>
2023-08-31 16:21:38 +0800

    Update FunctionIfTest.java

--- src/test/java/cn/cerc/mis/math/FunctionIfTest.java
+++ src/test/java/cn/cerc/mis/math/FunctionIfTest.java
@@ -12,4 +12,12 @@ public class FunctionIfTest {
         assertEquals("11", ff.process(null, "true,11,2"));
     }
 
+    @Test
+    public void test_ifAndMath() {
+        FunctionManage manage = new FunctionManage();
+        manage.addFunction(new FunctionIf());
+        manage.addFunction(new FunctionMath());
+        System.out.println(manage.parse("if(true,0,math(0/0*100))").getString());
+    }
+
 }
