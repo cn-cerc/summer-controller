8002741870b123fda2a0c44aed87e6c964fafff4
张弓 <sz9214e@qq.com>
2023-09-02 10:02:25 +0800

    Update FunctionValueTest.java

--- src/test/java/cn/cerc/mis/math/FunctionValueTest.java
+++ src/test/java/cn/cerc/mis/math/FunctionValueTest.java
@@ -8,7 +8,7 @@ public class FunctionValueTest {
 
     @Test
     public void test() {
-        FunctionData fv = new FunctionData("a(2)");
+        FunctionData fv = new FunctionData(null, "a(2)");
         assertEquals("a", fv.name());
         assertEquals("2", fv.param());
         assertEquals("a(2)", fv.text());
