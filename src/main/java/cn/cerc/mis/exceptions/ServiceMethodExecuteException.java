f33122843b02c9b4222b8393765703a2ebb9e638
封威 <1876381677@qq.com>
2024-05-13 15:02:18 +0800

    commit

---
+++ src/main/java/cn/cerc/mis/exceptions/ServiceMethodExecuteException.java
@@ -0,0 +1,25 @@
+package cn.cerc.mis.exceptions;
+
+import cn.cerc.mis.exception.IKnowall;
+
+public class ServiceMethodExecuteException extends RuntimeException implements IKnowall {
+
+    private String[] data = new String[0];
+
+    public ServiceMethodExecuteException(Throwable cause) {
+        super(cause);
+    }
+
+    // 运行异常, clientIP {}, token {}, service {}, corpNo {}, dataIn {}, message {}
+    public ServiceMethodExecuteException(String clientIP, String token, String corpNo, String dataIn,
+            Throwable cause) {
+        super(cause);
+        this.data = new String[] { clientIP, token, corpNo, dataIn };
+    }
+
+    @Override
+    public String[] getData() {
+        return data;
+    }
+
+}
