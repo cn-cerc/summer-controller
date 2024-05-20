9fd019ef784935c4eac715820e7cf297364df68d
HUANG Rongjun <l1091462907@gmail.com>
2024-05-20 06:59:03 +0000

    !649 add BookHandleException -> 不建议使用，请改进代码

---
+++ src/main/java/cn/cerc/mis/core/BookHandleException.java
@@ -0,0 +1,31 @@
+package cn.cerc.mis.core;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cn.cerc.mis.exception.IKnowall;
+
+/**
+ * 记录 BookHandle 的调用者异常
+ */
+public class BookHandleException extends RuntimeException implements IKnowall {
+
+    private static final long serialVersionUID = 5157802210363657558L;
+
+    private String corpNo;
+    private String userCode;
+
+    public BookHandleException(String corpNo, String userCode) {
+        this.corpNo = corpNo;
+        this.userCode = userCode;
+    }
+
+    @Override
+    public String[] getData() {
+        List<String> list = new ArrayList<String>();
+        list.add("corpNo: " + corpNo);
+        list.add("userCode: " + userCode);
+        return list.toArray(new String[0]);
+    }
+
+}
